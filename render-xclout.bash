e174cff6049e2f52b023ba008a1dac6811275d8d
Cedrick <70344513+Cedrick-J@users.noreply.github.com>
2023-12-16 07:41:49 +0800

    Update render-xclout.bash

--- render-xclout.bash
+++ render-xclout.bash
@@ -3,5 +3,6 @@ git clone -b flutter https://${GIT_USERANME}:${GIT_AUTH}@github.com/HaberTech/XC
 git clone -b backend https://${GIT_USERANME}:${GIT_AUTH}@github.com/HaberTech/XClout.git backend
 ls
 cd backend
+pip install -r requirements.txt 
 ls
 gunicorn app:app
