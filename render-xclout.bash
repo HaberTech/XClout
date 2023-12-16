5f44277217e09b630d77803b323d5fc24a3f91be
Cedrick <70344513+Cedrick-J@users.noreply.github.com>
2023-12-16 08:54:43 +0800

    Initial sepearte directories

--- render-xclout.bash
+++ render-xclout.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 pip install --upgrade pip
+git clone -b flutter https://${GIT_USERANME}:${GIT_AUTH}@github.com/HaberTech/XClout.git flutter
 git clone -b backend https://${GIT_USERANME}:${GIT_AUTH}@github.com/HaberTech/XClout.git backend
-git clone -b flutter https://${GIT_USERANME}:${GIT_AUTH}@github.com/HaberTech/XClout.git backend/flutter
 ls
 cd backend
 pip install -r requirements.txt 
